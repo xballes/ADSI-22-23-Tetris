1aac54da5543d28d99bc3cdd8063a173ee2abf38
xballes <70892571+xballes@users.noreply.github.com>
2022-12-03 10:36:00 +0100

    guardarPartida()

--- src/main/java/controlador/GestorPartida.java
+++ src/main/java/controlador/GestorPartida.java
@@ -26,21 +26,27 @@ public class GestorPartida {
 		System.out.println(fecha);
 		int puntuacion=pPartida.getNumLinesRemoved();
 		String nombreUsuario=pPartida.getNombreUsuario();
-		SGBD.getInstancia().execSQLVoid("INSERT INTO partida_guardada VALUES (%fecha%,%puntuacion%,%nivel%)");
+		//SGBD.getInstancia().execSQLVoid("INSERT INTO partida_guardada VALUES (%fecha%,%puntuacion%,%nivel%)");
 		int numcolumnas=pPartida.getBOARD_WIDTH();
 		int numaltura=pPartida.getBOARD_HEIGHT();
-		for(int i=0;i<numcolumnas;i++) {
+		int i =0;
+		while(i<numcolumnas) {
+			System.out.println("Columna nº"+i);
 			for(int j=0;j<numaltura;j++) {
-				String indice=String.valueOf(i);
-				String alt = "alt".concat(indice);
-				alt=String.valueOf(matriz[j][i]);
+				String indice=String.valueOf(j); //número de la columna
+				String alt=String.valueOf(matriz[j][i]); //alt1,alt2,alt3... por cada columna
 				System.out.println(alt);
-			}
+		}
 			//String sentenciaSQL = "INSERT INTO COLUMNA(usuario,fechaPartida,numcolumna,alt1,alt2,alt3,alt4,alt5,alt6,alt7,alt8,alt9,alt10,alt11,alt12,alt13,alt14,alt15,alt16,alt17,alt18,alt19,alt20,alt21,alt22) VALUES(%nombreUsuario%,%fecha%,%i%,%alt1%,%alt1%,%alt2%,%alt3%,%alt4%,%alt5%,%alt6%,%alt7%,%alt8%,%alt9%,%alt10%,%alt11%,%alt12%,%alt13%,%alt14%,%alt15%,%alt16%,%alt17%,%alt18%,%alt19%,%alt20%,%alt21%,%alt22%";
 			//SGBD.getInstancia().execSQLVoid(sentenciaSQL);
-		}
+			i++;
+			System.out.println("-------------------");
+			
+			
+	}
 		
 	}
 	
 	
 	}
+
