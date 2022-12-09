fcd0f4a444a6a542e4bede2e8ea1e42fabe73326
PaulaPinto001 <ppinto001@ikasle.ehu.eus>
2022-12-09 09:37:58 +0100

    creadas clases consultarPremios y detallesPremio, constructora GestroPremios

--- src/main/java/controlador/GestorPremios.java
+++ src/main/java/controlador/GestorPremios.java
@@ -2,4 +2,17 @@ package controlador;
 
 public class GestorPremios {
 
+private static GestorPremios puntero;	
+	
+	
+	private GestorPremios() {}
+	
+	public static GestorPremios getInstancia()
+	{
+		if (GestorPremios.puntero == null) {GestorPremios.puntero = new GestorPremios();}
+		return GestorPremios.puntero;
+	}
+	
+	
+	
 }
