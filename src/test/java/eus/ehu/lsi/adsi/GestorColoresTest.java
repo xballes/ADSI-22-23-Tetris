2120da742494dd5724cf9e705b385a3e8b813922
mikelp <mikel.pedrolinaje@gmail.com>
2022-12-16 10:50:31 +0100

    JUnits hechas

--- src/test/java/eus/ehu/lsi/adsi/GestorColoresTest.java
+++ src/test/java/eus/ehu/lsi/adsi/GestorColoresTest.java
@@ -20,14 +20,34 @@ public class GestorColoresTest {
 
 	@Test
 	public void testColores() {
-		//Caso 1: Seleccionar color
+		//Caso 1: Seleccionar  1er color
 	
 		this.gestor1.registrar("Per1", "Hola1", "gg");	
-		gestor.cambiarColor("Per1", 1);
+		gestor.cambiarColor("Per1", 0);
 		int id=	gestor.obtenerColor("Per1");
+		assertTrue(id==0);
+		//Caso 2: Seleccionar  2o color
+		gestor.cambiarColor("Per1", 1);
+		 id=	gestor.obtenerColor("Per1");
 		assertTrue(id==1);
+		//Caso 3: Seleccionar  3er color
+		gestor.cambiarColor("Per1", 2);
+		 id=	gestor.obtenerColor("Per1");
+		assertTrue(id==2);
+		//Caso 4: Seleccionar  4o color
 		
+		gestor.cambiarColor("Per1", 3);
+	 id=	gestor.obtenerColor("Per1");
+		assertTrue(id==3);
+		
+		//Caso 5: Seleccionar un color que ya estaba en la BD
+		
+		gestor.cambiarColor("Per1", 3);
+		 id=	gestor.obtenerColor("Per1");
+			assertTrue(id==3);
 		
 	}
+	
+	
 
 }
