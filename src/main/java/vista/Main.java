06f5f4fc0e6553702c9e48e8305c698c436b074e
Elsab <Elsab@192.168.1.21>
2022-12-18 13:54:21 +0100

    Junits de premios en Final

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -1,13 +1,20 @@
 package vista;
 
-import java.sql.Timestamp;
-
-import controlador.GestorPremios;
 
 public class Main {
 	
 	
-	// Teoricamente no interfaz, solo hace la llamada a la primera interfaz
+	/* Teoricamente no interfaz, solo hace la llamada a la primera interfaz
+	   Para que funcione el programa vincular las librerias en la carpeta lib con el proyecto
+	   Para ello hacer click derecho en "ADSI-22-23-Tetris" en el Proyect Explorer, elegir properties
+	   (última opción del menu) Elegir java build path (quinta opcion), moverse a la pestaña de libraries.
+	   Despues de esto pinchar en classpath y add External Jars (en ese orden). Elegir las dos librerias de la
+	   carpeta lib y "Apply and close".
+	 	
+	 
+	 
+	 
+	 */
 	
 	public static void main (String args[]) {
 	
---
+++ src/test/java/eus/ehu/lsi/adsi/GestorPremiosTest.java
@@ -0,0 +1,86 @@
+package eus.ehu.lsi.adsi;
+
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+
+import org.junit.Test;
+
+import controlador.GestorPremios;
+import controlador.GestorUsuarios;
+
+public class GestorPremiosTest {
+
+	
+	private GestorPremios gestorP;
+	private GestorUsuarios gestorU;
+	
+	public GestorPremiosTest () {
+		this.gestorP = GestorPremios.getInstancia();
+		this.gestorU = GestorUsuarios.getInstancia();
+		
+	}
+	
+	
+	private void reset() {
+		gestorU.borrarTodosLosUsers();
+	}
+	
+	
+	@Test
+	public void testConsultarPremios() {
+
+		this.reset();
+		gestorU.registrar("pepe", "hdh", "hdhd");
+		gestorU.registrar("pablo", "hdh", "dhdh");
+		
+		
+		// Caso 1: No hay premios otorgados
+		assertTrue(this.gestorU.obtenerPremios("pepe").length==0);
+		assertTrue(this.gestorU.obtenerPremios("pablo").length==0);
+		
+		// Caso 2: Un premio otorgado pero no al user pepe
+		Long datetime = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(datetime);
+		gestorP.darPremio("pablo", 1, timestamp);
+		
+		assertTrue(this.gestorU.obtenerPremios("pepe").length==0);
+		assertTrue(this.gestorU.obtenerPremios("pablo").length==1);
+		assertTrue(this.gestorP.tieneElPremio("pablo", 1));
+
+		
+		// Caso 3: Varios premios otorgados pero no al user pepe
+		gestorP.darPremio("pablo", 2, timestamp);
+		gestorP.darPremio("pablo", 3, timestamp);
+		assertTrue(this.gestorU.obtenerPremios("pepe").length==0);
+		assertTrue(this.gestorU.obtenerPremios("pablo").length==3);
+
+		assertTrue(this.gestorP.tieneElPremio("pablo", 2));
+		assertTrue(this.gestorP.tieneElPremio("pablo", 3));
+		assertTrue(this.gestorP.tieneElPremio("pablo", 1));
+
+		
+		// Caso 4: Un premio otorgado y es al user pepe
+		this.reset();
+		gestorU.registrar("pepe", "hdh", "hdhd");
+		gestorU.registrar("pablo", "dh", "dhdh");
+		gestorP.darPremio("pepe", 1, timestamp);
+		assertTrue(this.gestorU.obtenerPremios("pepe").length==1);
+		assertTrue(this.gestorU.obtenerPremios("pablo").length==0);
+
+		assertTrue(this.gestorP.tieneElPremio("pepe", 1));
+
+		
+		// Caso 5: Varios premios otorgados y es al user pepe
+		
+		gestorP.darPremio("pepe", 2, timestamp);
+		gestorP.darPremio("pepe", 0, timestamp);
+		assertTrue(this.gestorU.obtenerPremios("pepe").length==3);
+
+		assertTrue(this.gestorP.tieneElPremio("pepe", 1));
+		assertTrue(this.gestorP.tieneElPremio("pepe", 0));
+		assertTrue(this.gestorP.tieneElPremio("pepe", 2));
+
+		
+	}
+}
