21349fc08fbc35e0cedd35fc11ffed8d550538f0
Ivan <81016431+IvanMataFernandez@users.noreply.github.com>
2022-12-18 20:14:19 +0100

    Update Main.java

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -5,12 +5,15 @@ public class Main {
 	
 	
 	/* Teoricamente no interfaz, solo hace la llamada a la primera interfaz
+	
+	
 	   Para que funcione el programa vincular las librerias en la carpeta lib con el proyecto
 	   Para ello hacer click derecho en "ADSI-22-23-Tetris" en el Proyect Explorer, elegir properties
-	   (última opción del menu) Elegir java build path (quinta opcion), moverse a la pestaña de libraries.
+	   (Ãºltima opciÃ³n del menu) Elegir java build path (quinta opcion), moverse a la pestaÃ±a de libraries.
+	   Quitar del classpath las librerÃ­as que figuran en "missing" y aÃ±adir las nuevas
 	   Despues de esto pinchar en classpath y add External Jars (en ese orden). Elegir las dos librerias de la
 	   carpeta lib y "Apply and close".
-	 	
+	   	 	
 	 
 	 
 	 
